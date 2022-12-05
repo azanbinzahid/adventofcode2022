ef8df83f679345819bee6e011547fc1a7d0ee9a0
Azan Bin Zahid <binzahid@amazon.com>
2022-12-05 23:35:39 +0100

    sol: add day05

---
+++ day05/p2.py
@@ -0,0 +1,38 @@
+stacks = {}
+with open('in.txt', 'r') as f:
+
+    for line in f.readlines():
+        # start reading in chunks of 4 e.g. "[X] "
+        crates = list(map(''.join, zip(*[iter(line)]*4)))
+
+        # if it is empty line or the labels from crate stacks, do nothing
+        if crates == [] or crates[0] == ' 1  ':
+            continue
+        
+        # if its not a move statement, then it is a crate in the stack
+        elif crates[0] != 'move':
+            for i, crate in enumerate(crates):
+                i = i+1
+                crate = crate.strip().strip("]").strip("[")
+
+                # skip if no crate in stack
+                if crate == "":
+                    pass
+                else:
+                    stacks.setdefault(i, []).insert(0, crate)
+
+        else:
+            # we know we will get three ints in this line. Use regex for better readability
+            move, frm, to = [int(s) for s in line.split() if s.isdigit()]
+
+            # move crates but preserve the order
+            to_move = []
+            for m in range(move):
+                to_move.insert(0, stacks[frm].pop())
+            
+            stacks[to].extend(to_move)
+
+# sort the stacks based on the key (can also be handled while building stacks)
+stacks = sorted(stacks.items(), key=lambda x: x[0])
+ans = "".join([x[-1] for _, x in stacks])
+print(ans)
