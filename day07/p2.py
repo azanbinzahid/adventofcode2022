61612d8c3fa2111d38f14655157ce2a99b6bd416
Azan Bin Zahid <binzahid@amazon.com>
2022-12-07 11:51:26 +0100

    sol: add day07

---
+++ day07/p2.py
@@ -0,0 +1,129 @@
+class Node:
+    def __init__(self, name="/", root=None, size = 0, typ = None) -> None:
+        self.root = root
+        self.name = name
+        self.child = set()
+        self.size = size
+        self.type = typ
+        self.total_size = 70000000
+        self.req = 30000000
+        self.unused = None
+        self.min = None
+
+    def add_child(self, Node):
+        self.child.add(Node)
+        # self.size += Node.size
+
+    def print(self, indent=0):
+        print(f"{indent*' '} - {self.name} ({self.type}, size={self.size})")
+        for c in self.child:
+            c.print(indent=indent+2)
+
+    def add_size(self, size):
+        self.size += size
+
+    def get_node(self, name):
+        if name == self.name:
+            return self
+        
+        for c in self.child:
+            return c.get_node(c, name)
+
+    def calc_dir_size(self):
+        sum = 0
+        
+        if self.type == 'file':
+            sum += self.size
+
+        for c in self.child:
+            sum += c.calc_dir_size()
+            
+        self.size = sum
+        self.unused = self.total_size - self.size
+        self.min = self.req - self.unused
+        return sum
+
+
+    def query_1(self):
+        """
+        Find all of the directories with a total size of at most 100000,
+        then calculate the sum of their total sizes.
+        """
+
+        sum = 0
+        
+        if self.type == 'dir' and self.size <= 100000:
+            sum += self.size
+
+        for c in self.child:
+            sum += c.query_1()
+
+        return sum
+
+    def query_2(self, root, ans):
+        """
+        Find the smallest directory that, if deleted, would free up enough space on the
+        filesystem to run the update
+        """
+
+        if self.type == 'dir' and self.size > root.min:
+            ans.append(self.size)
+
+        for c in self.child:
+            c.query_2(root, ans)
+
+
+
+TOTAL_SIZE = 70000000
+root = None
+curr_path = None
+with open('in.txt', 'r') as f:
+    for line in f.readlines():
+        # check if command or output
+        if line.startswith("$"):
+
+            # remove $ in start
+            command = line.replace("$ ", "")
+
+            # check if cd
+            if command.startswith("cd"):
+
+                # get path
+                _, path = command.split()
+
+                # create a new node if not ..
+                if path == "..":
+                    curr_path = curr_path.root
+                else:
+                    node = Node(root=curr_path, name = path, typ="dir")
+
+                    # special case for root node
+                    if path == "/":
+                        root = node                        
+                    else:
+                        curr_path.add_child(node)
+
+                    # update current path
+                    curr_path = node
+
+            # else ls, skip for now
+            else:
+                pass
+        
+        # output
+        else:
+            # if listing dir, skip for now
+            if line.startswith("dir"):
+                pass
+
+            else:
+                size, name = line.split()
+                node = Node(name=name, size = int(size), root=curr_path, typ = "file")
+                curr_path.add_child(node)
+
+            
+root.calc_dir_size()
+
+ans = []
+root.query_2(root, ans)
+print(min(ans))
